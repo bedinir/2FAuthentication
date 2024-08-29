0001beb840da9915b1d5622276b006d77b8a0b82
Rina Bedini <rina@e-soft.al>
2024-08-29 12:15:42 +0200

    routes config

--- TwoFactorAuthApp/src/app/app.routes.ts
+++ TwoFactorAuthApp/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { Verify2faComponent } from './auth/verify2fa/verify2fa.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login',
+        component: LoginComponent
+    },
+    {
+        path:'register',
+        component: RegisterComponent
+    },
+    {
+        path:'verify-2fa',
+        component:Verify2faComponent
+    }
+];
