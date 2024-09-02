35682b372ea0aadcd22a664ea18f01cbebef36cd
Rina Bedini <rina@e-soft.al>
2024-09-02 15:20:14 +0200

    added profile settings and user details

--- TwoFactorAuthApp/src/app/home/home.component.ts
+++ TwoFactorAuthApp/src/app/home/home.component.ts
@@ -2,14 +2,19 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProfileComponent } from "./profile/profile.component";
 import { UserDetailsComponent } from "./user-details/user-details.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, ProfileComponent, UserDetailsComponent],
+  imports: [RouterModule, ProfileComponent, UserDetailsComponent,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  currentSection: string = 'home'; // Default section
 
+  showSection(section: string) {
+    this.currentSection = section;
+  }
 }
--- TwoFactorAuthApp/src/app/home/profile/profile.component.ts
+++ TwoFactorAuthApp/src/app/home/profile/profile.component.ts
@@ -1,14 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { HomeComponent } from "../home.component";
 import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Verify2faComponent } from "../../auth/verify2fa/verify2fa.component";
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [HomeComponent,RouterModule],
+  imports: [HomeComponent, RouterModule, ReactiveFormsModule, Verify2faComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  changePasswordForm: FormGroup;
+  twoFactorAuthForm: FormGroup;
+  is2FAEnabled: boolean = true;
+  showVerifyComponent: boolean = false;
+  showEnableModal: boolean = false;
 
+
+  constructor(private fb: FormBuilder,
+    private authService: AuthService
+  ) { 
+    this.changePasswordForm = this.fb.group({
+      OldPassword: [''],
+      NewPassword: [''],
+      RepeatNewPassword: ['']
+    });
+
+    this.twoFactorAuthForm = this.fb.group({
+      enable2FA: [this.is2FAEnabled]
+    });
+
+  }
+
+
+  ngOnInit(): void {
+    this.get2FAStatus();
+  }
+
+  onChangePassword(){}
+
+  get2FAStatus() {
+    // Fetch the 2FA status from the backend
+    // this.authService.get2FAStatus().subscribe((status: boolean) => {
+    //   this.is2FAEnabled = status;
+    //   this.twoFactorAuthForm.get('enable2FA')?.setValue(true);
+    // });
+    this.twoFactorAuthForm.get('enable2FA')?.setValue(true);
+
+  }
+
+  onCheckboxChange(event: any) {
+    if (!this.is2FAEnabled && event.target.checked) {
+      // Show modal to enable 2FA if it's currently disabled
+      this.showEnableModal = true;
+    }
+  }
+
+  onToggle2FA() {
+    if (!this.is2FAEnabled) {
+      // If enabling 2FA, show the verification component
+      this.showVerifyComponent = true;
+      this.showEnableModal = false;
+    } else {
+      // Disable 2FA
+      this.disable2FA();
+    }
+  }
+
+  disable2FA() {
+    // Logic to disable 2FA via backend call
+    // this.authService.disable2FA().subscribe(() => {
+    //   this.is2FAEnabled = false;
+    //   this.showVerifyComponent = false;
+    // });
+  }
+
+  onVerify() {
+    // Logic for verifying the 2FA code
+    // this.authService.verify2FA(this.twoFactorAuthForm.value.token,username).subscribe(() => {
+    //   this.is2FAEnabled = true;
+    //   this.showVerifyComponent = false;
+    // });
+  }
+
+  closeModal() {
+    this.showEnableModal = false;
+  }
 }
