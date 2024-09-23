0fc3af04929efa744fd0c837328d130480ebd7bc
Rina Bedini <rina@e-soft.al>
2024-09-23 11:27:06 +0200

    fix

--- TwoFactorAuthApp/src/app/guards/auth.guard.ts
+++ TwoFactorAuthApp/src/app/guards/auth.guard.ts
@@ -6,6 +6,8 @@ export const authGuard: CanActivateFn = (route, state) => {
   if (inject(AuthService).isAuthenticated()) {
     console.log('isAuth')
     return true;
+  } else {
+    inject(Router).navigate(['login']);
   }
 
   // inject(Router).navigate(['login']);
